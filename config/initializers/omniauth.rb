3458a9f226ce5bb0d9394bc24746ad23d6dc8def
simonekang <sk21007@tamu.edu>
2024-02-18 00:12:28 -0600

    production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     # Retrieve the Google credentials from Rails credentials
-    google_credentials = Rails.application.credentials.google
-  
     # Configure the Google OAuth provider with the client_id and client_secret
-    provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret], {
+    provider :google_oauth2, '363943209879-l76d1n3astafcvhf5482rshjkelu5hk5.apps.googleusercontent.com', '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', {
         scope: 'email, profile', # This grants access to the user's email and profile information.
         prompt: 'select_account', # This allows users to choose the account they want to log in with.
         image_aspect_ratio: 'square', # Ensures the profile picture is a square.
