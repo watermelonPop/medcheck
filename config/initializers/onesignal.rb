0ecda7b932cf1d00e1763f55cb4f8730c5c9f5df
Simone <sk21007@tamu.edu>
2024-03-23 21:42:31 -0500

    fix

--- config/initializers/onesignal.rb
+++ config/initializers/onesignal.rb
@@ -4,4 +4,5 @@ require 'onesignal'
 OneSignal.configure do |config|
   # Configure Bearer authorization: app_key
   config.app_key = 'NmYzZTEzMDgtZjZmYS00ZTI4LWJjOTYtMjFkNWM5ZDU1NTRi'
+  config.app_id = '90311568-0504-428b-9c92-0b1dda7bea46'
 end
