407650dd58aae76af82c3f518568fc26960b1252
Simone <sk21007@tamu.edu>
2024-05-28 00:08:42 -0500

    fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-        # Retrieve the Google credentials from Rails credentials
-        google_credentials = Rails.application.credentials.google
-      
-        # Configure the Google OAuth provider with the client_id and client_secret
-        provider :google_oauth2, google_credentials[:client_id], google_credentials[:client_secret], {
-            scope: 'email, profile', # This grants access to the user's email and profile information.
-            prompt: 'select_account', # This allows users to choose the account they want to log in with.
-            image_aspect_ratio: 'square', # Ensures the profile picture is a square.
-            image_size: 50, # Sets the profile picture size to 50x50 pixels.
-          }
+  google_credentials = Rails.application.credentials.google
+
+  provider :google_oauth2, google_credentials[:client_id], google_credentials[:client_secret], {
+    scope: 'email, profile',
+    prompt: 'select_account',
+    image_aspect_ratio: 'square',
+    image_size: 50,
+    access_type: 'offline',  # This ensures refresh tokens are received
+    redirect_uri: "#{ENV['HOST_URL']}/auth/google_oauth2/callback" # Ensure this matches your callback URL
+  }
 end
+
+OmniAuth.config.allowed_request_methods = [:post, :get] # Ensure GET is allowed
+OmniAuth.config.silence_get_warning = true # Silence the warning for allowing GET requests
